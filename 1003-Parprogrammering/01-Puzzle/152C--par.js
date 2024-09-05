1fb769a28353815d865b64f78207085138949a51
JohnF <johnf@getacademy.no>
2024-09-05 15:18:02 +0200

    Edited/added code in parprogrammering 01-puzzle

--- 1003-Parprogrammering/01-Puzzle/152C--par.js
+++ 1003-Parprogrammering/01-Puzzle/152C--par.js
@@ -12,20 +12,59 @@
  ******************************************************************************/
 let numbers = [1, 2, 5, 7, null, 3, 4, 8, 6];
 
-
+function shuffleArray(array) {
+  array.sort(() => Math.random() - 0.5)
+}
 /******************************************************************************
  * VIEW
  ******************************************************************************/
 function updateView() {
-    
+  const puzzleDiv = document.getElementById('puzzle');
+  puzzleDiv.innerHTML = '';
+
+  for (let i = 0; i < numbers.length; i++) {
+    const tile = document.createElement('div');
+    tile.className = 'puzzle__tile';
+
+    if (numbers[i] !== null) {
+      tile.innerHTML = numbers[i];
+      tile.onclick = function () {
+        swap(i);
+      };
+    } else {
+      tile.className += ' puzzle__tile--blank';
+    }
+    puzzleDiv.appendChild(tile);
+  }
 }
 
 /******************************************************************************
  * CONTROLLER
  ******************************************************************************/
+function swap(index) {
+  const blankIndex = numbers.indexOf(null);
+  if (areNeighbours(index, blankIndex)) {
+    [numbers[index], numbers[blankIndex]] = [numbers[blankIndex], numbers[index]];
+    updateView();
+  }
+}
 
+function areNeighbours(index1, index2) {
+  const row1 = Math.floor(index1 / 3);
+  const col1 = index1 % 3;
+  const row2 = Math.floor(index2 / 3);
+  const col2 = index2 % 3;
 
+  return (row1 === row2 && Math.abs(col1 - col2) === 1) || (col1 === col2 && Math.abs(row1 - row2) === 1);
+}
 
+function startAgain() {
+  shuffleArray(numbers);
+  updateView();
+}
 
-
+shuffleArray(numbers);
 updateView();
+
+// Add event listener to the "Start Again" button
+document.getElementById('startAgainButton').addEventListener('click', startAgain);
